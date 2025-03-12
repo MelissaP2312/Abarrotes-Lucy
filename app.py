f217de537c662b2d71323adb413149e9ce08d1be
Melissa Carolina Palma Sosa <150838731+MelissaP2312@users.noreply.github.com>
2025-03-11 23:00:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def send_message():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
